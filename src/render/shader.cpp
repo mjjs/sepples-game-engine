dcfbe58d1c92204ddf7c042067e7aba050feddd3
Mikko Siitonen <misiitone@gmail.com>
2020-07-11 22:20:55 +0300

    Fix linter stuff

--- src/render/shader.cpp
+++ src/render/shader.cpp
@@ -1,7 +1,6 @@
 #include "shader.h"
 #include <stdexcept>
 #include <string>
-#include <iostream>
 #include <utility>
 
 Shader::Shader() :
--- src/render/shader.h
+++ src/render/shader.h
@@ -17,28 +17,33 @@ enum ShaderErrorCheckType {
 
 class Shader {
     private:
-         GLuint shader_program;
-         std::vector<GLuint> attached_shaders{};
-         std::unordered_map<std::string, GLint> uniform_variables{};
+        GLuint shader_program;
+        std::vector<GLuint> attached_shaders{};
+        std::unordered_map<std::string, GLint> uniform_variables{};
 
-         void add_program(const std::string& shader_name, GLenum shader_type);
-         static std::pair<bool, std::string> check_shader_error(ShaderErrorCheckType check_type,
-                 GLenum check_name, GLuint to_check);
+        void add_program(const std::string& shader_name, GLenum shader_type);
+        static std::pair<bool, std::string> check_shader_error(ShaderErrorCheckType check_type,
+                GLenum check_name, GLuint to_check);
     public:
-         Shader();
-         ~Shader();
+        Shader();
+        ~Shader();
 
-         void add_vertex_shader(const std::string& shader_name);
-         void add_geometry_shader(const std::string& shader_name);
-         void add_fragment_shader(const std::string& shader_name);
-         void compile_shader();
+        Shader(const Shader&) = delete;
+        Shader(const Shader&&) = delete;
+        Shader& operator=(const Shader&) = delete;
+        Shader& operator=(const Shader&&) = delete;
 
-         void add_uniform(const std::string& variable_name);
-         void set_uniform(const std::string& variable_name, int value);
-         void set_uniform(const std::string& variable_name, float value);
-         void set_uniform(const std::string& variable_name, const Math::Vector3& vector);
-         void set_uniform(const std::string& variable_name, const Math::Matrix4& matrix);
+        void add_vertex_shader(const std::string& shader_name);
+        void add_geometry_shader(const std::string& shader_name);
+        void add_fragment_shader(const std::string& shader_name);
+        void compile_shader();
 
-         void bind();
+        void add_uniform(const std::string& variable_name);
+        void set_uniform(const std::string& variable_name, int value);
+        void set_uniform(const std::string& variable_name, float value);
+        void set_uniform(const std::string& variable_name, const Math::Vector3& vector);
+        void set_uniform(const std::string& variable_name, const Math::Matrix4& matrix);
+
+        void bind();
 };
 #endif
