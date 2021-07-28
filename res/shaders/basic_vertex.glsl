8f35cf843190107dece7f5a1fb02a02065bd030f
Mikko Siitonen <misiitone@gmail.com>
2021-07-28 18:13:38 +0300

    Change opengl version to 4.6

--- res/shaders/basic_vertex.glsl
+++ res/shaders/basic_vertex.glsl
@@ -1,4 +1,4 @@
-#version 330 core
+#version 460 core
 
 layout(location = 0) in vec3 position_i;
 layout(location = 1) in vec3 normal_i;
--- src/render/window.cpp
+++ src/render/window.cpp
@@ -24,8 +24,8 @@ Window::Window(const size_t width, const size_t height, const std::string& title
     }
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     SDL_GL_SetSwapInterval(1);
